594974c82efbd4c2721f06d7a110323ddd3a9701
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2025-10-18 08:23:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Events from './pages/Events';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/events" element={<Events />} />
     </Routes>
   );
 };
