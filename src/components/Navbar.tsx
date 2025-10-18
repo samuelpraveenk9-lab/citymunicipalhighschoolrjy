7410fd4784022fe9f79091fe207e4fae1abf523b
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2025-10-18 11:20:01 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,32 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+const NavBar: React.FC = () => {
   return (
-    <nav style={{ backgroundColor: "#004aad", padding: "10px" }}>
-      <h2 style={{ color: "white", margin: 0 }}>City Municipal High School</h2>
-      <div style={{ marginTop: "5px" }}>
-        <Link to="/" style={{ color: "white", marginRight: "15px" }}>Home</Link>
-        <Link to="/gallery" style={{ color: "white", marginRight: "15px" }}>Gallery</Link>
-        <Link to="/events" style={{ color: "white" }}>Events</Link>
-      </div>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: "20px",
+        padding: "15px",
+        backgroundColor: "#1e3a8a",
+        color: "#fff",
+        position: "sticky",
+        top: 0,
+        zIndex: 1000,
+      }}
+    >
+      <Link to="/" style={{ color: "#fff", textDecoration: "none", fontWeight: "bold" }}>
+        Home
+      </Link>
+      <Link to="/gallery" style={{ color: "#fff", textDecoration: "none", fontWeight: "bold" }}>
+        Gallery
+      </Link>
+      <Link to="/events" style={{ color: "#fff", textDecoration: "none", fontWeight: "bold" }}>
+        Events
+      </Link>
     </nav>
   );
-}
+};
+
+export default NavBar;
