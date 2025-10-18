537c8c81b3a0b144bce32751ac1d77deead84699
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2025-10-18 08:15:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173
+  }
 });
->>>>>>> 3e3e914 (Add new project with Home, Gallery, Events, and assets)
