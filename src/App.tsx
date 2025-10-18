2b2607135c938f8ca8d7bfbc55efdb5a7e4b137e
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2025-10-18 11:21:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Gallery from './pages/Gallery';
-import Events from './pages/Events';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Events from "./pages/Events";
+import NavBar from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/events" element={<Events />} />
-    </Routes>
+    <Router>
+      {/* Navigation Bar */}
+      <NavBar />
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/events" element={<Events />} />
+        {/* Optional: redirect unknown routes to Home */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
