37e8eab10c0e6706e605e116c6342ccf47f7ac38
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2025-10-18 08:20:15 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Gallery from "./pages/Gallery";
-import Events from "./pages/Events";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/events" element={<Events />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
