e706db1181ec77e6a8ab31bfa4886f3fb516b5d5
samuelpraveenk9-lab <samuelpraveenk9@gmail.com>
2026-06-20 22:28:06 +0530

    Fix import path for Footer component in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import hmPhoto from "../assets/hm-photo.jpg";
 import issue4cover from "../assets/issue-4-cover.png";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
 const stats = [
